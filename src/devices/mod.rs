b20f9200a0f45b742f0b9df69fb263c16b0cb4c7
Mike <mishakozlov74@gmail.com>
2023-07-19 23:08:22 +0200

    Fixing device factory

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -1,5 +1,5 @@
 pub mod device_ally;
-
+use std::fs;
 use device_ally::DeviceAlly;
 use crate::server::SettingsRequest;
 
@@ -16,14 +16,28 @@ pub trait Device {
 
 pub fn create_device() -> Option<Box<dyn Device>> {
     let product_name_path = "/sys/devices/virtual/dmi/id/product_family";
-    let product_name = std::fs::read_to_string(product_name_path)
+    let product_name = fs::read_to_string(product_name_path)
         .expect("Couldn't read product_family");
 
-    println!("Device Name: {:?}", product_name.trim());
-    println!("Is Rog : {:?}", product_name.trim() == "ROG Ally");
+    println!("{}", get_device_name());
 
     match product_name.trim().as_ref() {
         "ROG Ally" => Some(Box::new(DeviceAlly)),
         _ => None,
     }
+}
+
+fn get_device_name() -> String {
+    let cpuinfo = fs::read_to_string("/proc/cpuinfo").expect("Unknown");
+
+    let model_re = Regex::new(r"model name\s*:\s*(.*)").unwrap();
+    let model = model_re.captures_iter(&cpuinfo).next().unwrap()[1].trim().to_string();
+
+    let board_vendor = fs::read_to_string("/sys/devices/virtual/dmi/id/board_vendor")
+        .expect("Unknown").trim().to_string();
+
+    let board_name = fs::read_to_string("/sys/devices/virtual/dmi/id/board_name")
+        .expect("Unknown").trim().to_string();
+
+    format!("{} {} {}", model, board_vendor, board_name)
 }
