3f944d4dc18904a8993f07acf9227c7b2b55c10a
Mike <mishakozlov74@gmail.com>
2023-07-19 22:49:07 +0200

    Fixing device factory

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -17,7 +17,10 @@ pub trait Device {
 pub fn create_device() -> Option<Box<dyn Device>> {
     let product_name_path = "/sys/devices/virtual/dmi/id/product_family";
     let product_name = std::fs::read_to_string(product_name_path)
-        .expect("Ally");
+        .expect("Couldn't read product_family");
+
+    println!("Device Name: {:?}", product_name.trim());
+    println!("Is Rog : {:?}", product_name.trim() == "ROG Ally");
 
     match product_name.trim().as_ref() {
         "ROG Ally" => Some(Box::new(DeviceAlly)),
