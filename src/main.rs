52c0ad73d2b1ef2ff7cd34f1ba31250b78886088
Mike <mishakozlov74@gmail.com>
2023-07-23 01:42:08 +0200

    [Research]: virtual device

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod server;
 mod steam;
 mod devices;
 mod utils;
+use evdev_rs::enums::EV_KEY;
+use evdev_rs::enums::EventCode;
+use std::time::Duration;
+use uinput::{event::keyboard, CreateDevice, Device, Key};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -20,6 +24,25 @@ async fn main() -> std::io::Result<()> {
     for thread in threads {
         thread.join().unwrap();
     }
+
+    let mut device = CreateDevice::new()?
+        .name("virtual_keyboard")?
+        .with_events(vec![
+            // Add the keys that you want to emit events for.
+            keyboard::Key::A.into(),
+        ])?
+        .create()?;
+
+    // Simulate a key press.
+    device.emit(&Key::A, 1)?;
+    device.synchronize()?;
+
+    // Wait a bit.
+    std::thread::sleep(Duration::from_millis(100));
+
+    // Simulate a key release.
+    device.emit(&Key::A, 0)?;
+    device.synchronize()?;
 
     Ok(())
 }
